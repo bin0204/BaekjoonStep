a8a7e5ce0cb3c3b514bd9c11f564d15acf5a7956
bin0204 <bin0204@github.com>
2021-04-21 20:13:39 +0900

    Cat

---
+++ 10171.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args){
+        
+		StringBuffer cat = new StringBuffer();
+		cat.append("\\    /\\\n"); // \n newline
+		cat.append(" )  ( ')\n");
+		cat.append("(  /  )\n");
+ 		cat.append(" \\(__)|\n");
+		
+		System.out.println(cat);
+	}
+}
